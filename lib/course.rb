f615fcefb6285d4b96856cc71451d77553f2985e
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2020-11-27 17:45:49 +0000

    Update main.user.js

--- lib/course.rb
+++ lib/course.rb
@@ -3,4 +3,5 @@ module George
     attr_accessor :term_id, :department_id, :id
 
     def initialize(attributes)
-      @term_id = attributes[:term_id] # 20150
+      @term_id = attributes[:term_id] # 201503
+      @department_id
