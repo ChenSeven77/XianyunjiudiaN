8d2eef4a6ba0aa2cf21517cbe6d570b0136d0fc5
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2020-12-12 13:15:39 +0000

    ids

--- lib/george.rb
+++ lib/george.rb
@@ -5,3 +5,12 @@ require 'nokogiri'
 require 'csv'
 require 'fileutils'
 #require 'active_support/core_ext/object/try'
+
+require_relative "term.rb"
+require_relative "course.rb"
+require_relative "section.rb"
+require_relative "student.rb"
+
+module George
+  TERMS_PATH = File.expand_path("../../terms", __FILE__)
+end
---
+++ lib/section.rb
@@ -0,0 +1,2 @@
+module George
+  cl
