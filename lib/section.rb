e622589cf6675acbc2989817832145cdbaa920ae
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2021-03-31 13:01:03 +0000

    nicer logging

--- lib/section.rb
+++ lib/section.rb
@@ -142,4 +142,4 @@ module George
       @students = self.students
 
       CSV.open(student_report_path, "w", :write_headers=> true, :headers => @students.first.keys.map{|k| k.to_s}) do |csv|
-        @
+        @students.each do |student_attribu
