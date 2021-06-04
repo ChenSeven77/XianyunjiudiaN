2e8bcfb53e87c5d3586b7c968598e3c40beffde8
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2021-06-04 12:09:24 +0000

    [API] Working on API Docs

--- lib/section.rb
+++ lib/section.rb
@@ -189,4 +189,5 @@ module George
       tables = document.css("table")
       data_tables = tables.select{|t| t.attributes["class"] && t.attributes["class"].value == "datadisplaytable"}
       #course_summary_table     = data_tables.find{|t| t.attributes["summary"] && t.attributes["summary"].value == "This table displays the attributes of the course." }
-      #enrollment_summary_table = data_tables.find{|t| t.attributes["summary"] && t.attributes["summary"].value == "This table displays enrollment and waitlist coun
+      #enrollment_summary_table = data_tables.find{|t| t.attributes["summary"] && t.attributes["summary"].value == "This table displays enrollment and waitlist counts." }
+      enrollments_
