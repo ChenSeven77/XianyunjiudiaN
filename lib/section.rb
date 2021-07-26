1fb6ac2a3c9dc2c67f83f67442d36eee8640ed93
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2021-07-26 21:14:14 +0000

    more filters

--- lib/section.rb
+++ lib/section.rb
@@ -221,4 +221,5 @@ module George
 
         # Parse email link
 
-        student_email_address = email_link.attributes["href"].value.gsub("mailto:","") #net_id = email_addres
+        student_email_address = email_link.attributes["href"].value.gsub("mailto:","") #net_id = email_address.gsub("@gwu.edu")
+        #student_
