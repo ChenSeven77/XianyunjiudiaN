f307fadb009d90d98cb44b531f410248e04f5023
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2021-10-03 10:04:47 +0000

    0.1.39

--- lib/section.rb
+++ lib/section.rb
@@ -285,4 +285,4 @@ module George
 
       document = Nokogiri::HTML(open(download_class_details_path))
       tables = document.css("table")
-      data_tabl
+      data_tables = tables.select{|t| t.attributes["c
