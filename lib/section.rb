096aa1519e63d692451cd2b230e46b9844543e58
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2021-10-17 21:18:26 +0000

    smart

--- lib/section.rb
+++ lib/section.rb
@@ -288,4 +288,4 @@ module George
       data_tables = tables.select{|t| t.attributes["class"] && t.attributes["class"].value == "datadisplaytable"}
       #course_summary_table = data_tables.find{|t| t.attributes["summary"] && t.attributes["summary"].value == "This table displays the attributes of the course." }
       #enrollment_summary_table = data_tables.find{|t| t.attributes["summary"] && t.attributes["summary"].value == "This table displays enrollment and waitlist counts." }
-      students_table = data_tables.fin
+      students_table = data_tables.find{|t| t.attributes["summary"] 
