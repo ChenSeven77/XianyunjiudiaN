8348c8dc06f0c01d4389ebd16a02a0698a3040f8
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2022-10-29 09:55:27 +0000

    Increase test timeout

--- lib/section.rb
+++ lib/section.rb
@@ -444,4 +444,4 @@ module George
 
         ###expected_student_keys = [:admit_term, :admit_type, :campus, :catalog_term, :college, :degree, :email_address, :full_name, :gwid, :level, :major, :notification_expires, :record_number, :registration_number, :registration_status, :waitlist_position]
         ###if save_next == true
-        ###  #ra
+        ###  #raise KeyMismatchError.new(e
