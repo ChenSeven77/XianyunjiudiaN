86067506eaf36d69666191edde4939495ec062df
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2022-12-25 22:07:09 +0000

    Prepare 0.3.6 release

--- lib/term.rb
+++ lib/term.rb
@@ -4,3 +4,30 @@ module George
     attr_accessor :id, :name, :start_date, :end_date, :tentative_finals_schedule_url
 
     def initialize(attributes)
+      @id = attributes[:id]
+      @name = attributes[:name]
+      @start_date = attributes[:start_date]
+      @end_date = attributes[:start_date]
+      @tentative_finals_schedule_url = attributes[:tentative_finals_schedule_url]
+    end
+
+    def self.current
+      terms_dir = Dir.new(TERMS_PATH)
+      term_ids = terms_dir.entries.reject{|t| t.include?(".")} # converts [".", "..", "201502", "201503"] to ["201502", "201503"]
+      term_ids = term_ids.sort #.sort_by(&:to_i) # sorts in ascending order
+      term_id = term_ids.last
+      term_file = File.join(TERMS_PATH, term_id, "term.json")
+      term_file_contents = File.read(term_file)
+      term_attributes = JSON.parse(term_file_contents, :symbolize_names => true)
+      Term.new(term_attributes)
+    end
+
+    def courses_path
+      File.join(TERMS_PATH, self.id, "courses")
+    end
+
+    def courses_dir
+      Dir.new(self.courses_path)
+    end
+
+    def course_ids
