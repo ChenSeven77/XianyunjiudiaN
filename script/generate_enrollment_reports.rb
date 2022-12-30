575724cbb2856a695f95f0cbb79498609c1dc5a8
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2022-12-30 11:13:59 +0000

    Changed comment

--- script/generate_enrollment_reports.rb
+++ script/generate_enrollment_reports.rb
@@ -2,4 +2,6 @@
 # to run from the root directory: `ruby script/generate_enrollment_reports.rb`
 #
 
-# Generate enrollment report for each curren
+# Generate enrollment report for each current course.
+
+require_relative "../li
