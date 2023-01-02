df313e8ab91a0d9b71d69d4ec2c5a3434c9a500c
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2023-01-02 07:27:17 +0000

    Functions

--- script/generate_enrollment_reports.rb
+++ script/generate_enrollment_reports.rb
@@ -8,4 +8,5 @@ require_relative "../lib/george.rb"
 
 current_courses = George::Term.current.courses
 current_sections = current_courses.map{|course| course.sections }.flatten
-current_sections.each do |secti
+current_sections.each do |section|
+  section.generate_enro
