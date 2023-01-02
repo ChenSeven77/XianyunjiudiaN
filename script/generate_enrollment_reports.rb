4a638c985d04eb841932c15d2f05819ed525aa79
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2023-01-02 08:16:15 +0000

    new folder structure

--- script/generate_enrollment_reports.rb
+++ script/generate_enrollment_reports.rb
@@ -9,4 +9,4 @@ require_relative "../lib/george.rb"
 current_courses = George::Term.current.courses
 current_sections = current_courses.map{|course| course.sections }.flatten
 current_sections.each do |section|
-  section.generate_enro
+  section.generate_enrollment_report # generat
