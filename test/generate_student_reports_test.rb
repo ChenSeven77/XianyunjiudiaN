d80f51dbd0d3d29512ef6a51eec04c693eaacc98
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2023-01-12 05:53:49 +0000

    Some things

--- test/generate_student_reports_test.rb
+++ test/generate_student_reports_test.rb
@@ -1,3 +1,6 @@
 #
 # test for script/generate_student_reports.rb
-# to run from the root directory: `ruby test/generate_student_reports_te
+# to run from the root directory: `ruby test/generate_student_reports_test.rb`
+#
+
+require_relative "../lib/geo
