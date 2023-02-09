f20e9c9862c712cdeb6d42c57715cc02bf799833
ChenSeven77 <115212174+ChenSeven77@users.noreply.github.com>
2023-02-09 17:40:12 +0000

    Ignore .pyc files.

--- test/generate_student_reports_test.rb
+++ test/generate_student_reports_test.rb
@@ -16,4 +16,4 @@ current_sections.each do |section|
   section.generate_student_report
 
   test_results << (File.exist?(report_path) ?  true : false) # expect the report file to exist
-  test_results << (IO.readlines(report_path)
+  test_results << (IO.readlines(report_path).length >= 34 ? true : false) #todo: expe
